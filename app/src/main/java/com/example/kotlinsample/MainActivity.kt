025fe1cbc5ff14dccb08fb989fa9276428a128c0
na0 <skdud907@naver.com>
2023-08-28 23:34:28 +0900

    init

---
+++ app/src/main/java/com/example/kotlinsample/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        db.todoDao().getAll().observe(this, Observer {
+            result_text.text = it.toString()
+        })
+
+        add_button.setOnClickListener {
+            db.todoDao().insert(Todo(todo_edit.text.toString()))
+        }
+    }
+}
